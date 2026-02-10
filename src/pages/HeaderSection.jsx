9343da3c15840cef37374ff971139bdaeb419b47
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2026-02-10 11:08:08 +0000

    Added simple navigation to the web app

--- src/pages/HeaderSection.jsx
+++ src/pages/HeaderSection.jsx
@@ -6,14 +6,14 @@ export default function HeaderSection ({ onSelect }) {
     return (
         <div className="hidden md:flex py-5 px-5 items-center justify-between text-slate-100 border-b-1 border-slate-800/60">
             <nav className="h-full flex gap-5 items-center justify-center self-end relative z-2">
-                <a className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(1)}>ABOUT</a>
-                <a className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(2)}>PROJECTS</a>
-                <a className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(3)}>CONTACT</a>
+                <a href="#aboutMe" className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(1)}>ABOUT</a>
+                <a href="#projects" className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(2)}>PROJECTS</a>
+                <a href="#contactPage" className='hover:text-slate-300 cursor-pointer' onClick={() => onSelect(3)}>CONTACT</a>
             </nav>
-            <button className="text-slate-100 hover:text-violet-400 flex items-center gap-1 cursor-pointer">
+            <a href="#contactPage" className="text-slate-100 hover:text-violet-400 flex items-center gap-1 cursor-pointer">
                 <p className="border-b border-slate-100 hover:border-violet-400">Get In Touch</p>
                 <FontAwesomeIcon icon={faArrowRightLong} className="rotate-[-45deg]"/>
-            </button>
+            </a>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import AboutMe from './about-me/AboutMe'
 import ContactPage from './ContactPage'
-import HeaderSection from './HeaderSection'
 import HeroSection from './HeroSection'
 import Projects from './projects/Projects'
 
 function Homepage () {
-  
+
   return (
     <div className="md:px-10 bg-slate-900 p-5 md:p-0 flex flex-col md:gap-10">
-        <HeroSection/>
-        <AboutMe />
-        <Projects />
-        <ContactPage />
+        <section><HeroSection/></section>
+        <section id="aboutMe"><AboutMe /></section>
+        <section id='projects'><Projects /></section>
+        <section id='contactPage'><ContactPage /></section>
     </div>
   )
 }
